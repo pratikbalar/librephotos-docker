a64be4fb187f989ed4ed3fe387f1453d96d07df8
pratikbalar <pratik@improwised.com>
2021-09-18 01:16:35 +0530

    dockerfile: refactor dockerfiles, download models in entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,21 @@
-#! /bin/bash
-export PYTHONUNBUFFERED=TRUE
-export PYTHONFAULTHANDLER=1
+#!/bin/bash -e
+[[ "$DEBUG" ]] && set -x
+
+curl_flag='--connect-timeout 10 --max-time 10 --retry 5 --retry-delay 10 --retry-max-time 180 -SL'
+curl ${curl_flag} \
+  https://github.com/LibrePhotos/librephotos-docker/releases/download/0.1/places365.tar.gz | \
+  tar -zxC /data_models/ &
+curl ${curl_flag} \
+  https://github.com/LibrePhotos/librephotos-docker/releases/download/0.1/im2txt.tar.gz | \
+  tar -zxC /data_models/ &
+curl ${curl_flag}
+  https://github.com/LibrePhotos/librephotos-docker/releases/download/0.1/clip-embeddings.tar.gz | \
+  tar -zxC /data_models/ &
+curl ${curl_flag} https://download.pytorch.org/models/resnet152-b121ed2d.pth \
+  -o /root/.cache/torch/hub/checkpoints/resnet152-b121ed2d.pth &
+
+export PYTHONFAULTHANDLER=1 PYTHONUNBUFFERED=TRUE
+
 mkdir -p /logs
 service statd start
 python image_similarity/main.py 2>&1 | tee /logs/gunicorn_image_similarity.log &
@@ -11,15 +26,18 @@ python manage.py build_similarity_index 2>&1 | tee /logs/command_build_similarit
 python manage.py clear_cache
 python manage.py createadmin -u $ADMIN_USERNAME $ADMIN_EMAIL 2>&1 | tee /logs/command_createadmin.log
 
-echo "Running backend server..."
+echo "###############################"
+echo "#                             #"
+echo "#  Running backend server...  #"
+echo "#                             #"
+echo "###############################"
 
 python manage.py rqworker default 2>&1 | tee /logs/rqworker.log &
 
-if [ "$DEBUG" = 1 ]
-then
-    echo "development backend starting"
+if [[ "$DEBUG" == 1 ]]; then
+    echo "Development backend starting"
     gunicorn --worker-class=gevent --timeout 36000 --reload --bind 0.0.0.0:8001 --log-level=info ownphotos.wsgi 2>&1 | tee /logs/gunicorn_django.log
 else
-    echo "production backend starting"
+    echo "Production backend starting"
     gunicorn --worker-class=gevent --timeout 3600 --bind 0.0.0.0:8001 --log-level=info ownphotos.wsgi 2>&1 | tee /logs/gunicorn_django.log
 fi
